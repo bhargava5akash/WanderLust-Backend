1fe0ff6d9d522e5eb47a56a7a84dbb13d08839a6
bhargava5akash <surabhargavaakash@gmail.com>
2026-04-25 12:04:20 +0530

    fix cors

--- server.py
+++ server.py
@@ -708,7 +708,10 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("FRONTEND_URL", "http://localhost:3000")],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://wanderlust-frontend-9sl9.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
